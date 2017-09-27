acf572ce3e9f24de3405ada30fb3a9031320be9c
31206018 <31206018@099077-P31S204.macklabs.br>
2017-09-27 20:57:33 -0300

    Importando mais coisas2

--- Source Code Inspection/test/br/calebe/ticketmachine/test/PapelMoedaTest.java
+++ Source Code Inspection/test/br/calebe/ticketmachine/test/PapelMoedaTest.java
@@ -12,11 +12,7 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import org.junit.Before;
-import org.junit.Test;
 import static org.junit.Assert.*;
-
 /**
  *
  * @author 31206018
